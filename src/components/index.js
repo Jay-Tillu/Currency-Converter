6450640c148afb8a3128ec4afd32eff84249fe24
Jay Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2024-09-12 12:40:48 +0530

    Project Done

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// To Export all custom components at once. We use this method.
+// We import all the components here and then we export all from here.
+// RECOMMENDED FOR PRODUCTION
+
+import InputBox from "./Inputbox";
+
+export { InputBox }
+// export { InputBox, AnotherComponent }
